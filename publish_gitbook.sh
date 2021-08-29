8ec17e040744880d4747e1798370394995d3c1cd
sjchoi <sjchoi@casys.kaist.ac.kr>
2021-08-29 16:12:19 +0900

    upadte docs

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,6 +1,3 @@
-# gitbook 의존 파일을 설치하고 gitbook 빌드를 돌린다.
-gitbook install && gitbook build
-
 git add *
 git commit -m "upadte docs"
 git push
@@ -10,6 +7,8 @@ git checkout gh-pages
 
 # 최신 gh-pages 브랜치 정보를 가져와 rebase를 진행한다.
 git pull origin gh-pages --rebase
+
+gitbook install && gitbook build
 
 # gitbook build로 생긴 _book폴더 아래 모든 정보를 현재 위치로 가져온다.
 cp -R _book/* .
