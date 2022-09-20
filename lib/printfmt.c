d124b3de7f6a34fca3675c1bb1ef873b030b9ed6
Jinha Chung <jinha.chung@kaist.ac.kr>
2022-09-20 16:40:14 +0900

    lab1: 4/10 (exercise 9)

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -211,10 +211,18 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 			// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('X', putdat);
+			/*
+            putch('X', putdat);
 			putch('X', putdat);
 			putch('X', putdat);
 			break;
+            */
+            // exercise 8 - copy case for hex (case 'x')
+            //putch('0', putdat);
+            //putch('o', putdat);
+            num = getuint(&aq, 3);
+            base = 8;
+            goto number;
 
 			// pointer
 		case 'p':
